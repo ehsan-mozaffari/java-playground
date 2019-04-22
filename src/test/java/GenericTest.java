2229b6d43a3c8bd19f33ae04fd4205b5b692fa74
Ehsan Mozaffari <e.mozaffari@navaco.ir>
2019-04-22 17:09:37 +0430

    jUnit added

---
+++ src/test/java/GenericTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GenericTest {
+
+
+    @Test
+    public void testSample(){
+
+        Assert.assertTrue("Has Error", true);
+    }
+
+}
